2b0c35afcbbd5561fec6be100c7f03f1b9550cc2
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-08-10 18:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function insert_Row() {
 	b.textContent= "New Cell1"
 	a.appendChild(b)
 	a.appendChild(c)
-	abc.insertBefore(a, abc.lastChild)
+	abc.insertBefore(a, abc.firstChild)
 	//Write your code here
   
 }
