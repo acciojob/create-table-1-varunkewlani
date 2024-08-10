9a1a540b1f56b7a92eecc2a166c742306c19c669
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-08-10 17:46:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
-    //Write your code here
-  
+	let abc=document.getElementById("sampleTable")
+	let a=document.createElement("tr")
+	let b=document.createElement("td")
+	let c=document.createElement("td")
+	c.textContent= "New Cell2"
+	b.textContent= "New Cell1"
+	a.appendChild(b)
+	a.appendChild(c)
+	abc.insertBefore(a,abc.childNodes[0])
+	//Write your code here
   
 }
